3b00e8315a61f93929f8aafb6d714088d5e5a057
Ingvar Derešivski <inde999@gmail.com>
2023-03-08 08:57:39 +0200

    init

---
+++ 07.03.2023 LINQ/LINQ/LINQ/Program.cs
@@ -0,0 +1,123 @@
+﻿namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ");
+
+            //kutsume WhereLINQ meetodi välja
+            WhereLINQ();
+            ThenByLINQ();
+            ThenByDescendingLINQ();
+            ToLookUpLINQ();
+            JoinLINQ();
+        }
+
+        public static void WhereLINQ()
+        {
+            var peopleAge = PeopleList.peoples
+                .Where(x => x.Age > 20 && x.Age < 23);
+
+            foreach (var item in peopleAge)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+
+        public static void ThenByLINQ()
+        {
+            Console.WriteLine("ThenBy järgi reastamine");
+
+            var thenByResult = PeopleList.peoples
+                //järjestab nimede järgi ja siis vanuse järgi
+                .OrderBy(x => x.Name)
+                .ThenBy(x => x.Age);
+
+            foreach (var people in thenByResult)
+            {
+                Console.WriteLine(people.Name + " " + people.Age);
+            }
+        }
+
+        public static void ThenByDescendingLINQ()
+        {
+            Console.WriteLine("ThenByDescending");
+
+            var thenByDescending = PeopleList.peoples
+                .OrderBy(x => x.Name)
+                .ThenByDescending(x => x.Age);
+
+            foreach (var people in thenByDescending)
+            {
+                Console.WriteLine(people.Name + " " + people.Age);
+            }
+        }
+
+        public static void ToLookUpLINQ()
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("ToLookUp järgi reastamine");
+
+            var toLookUp = PeopleList.peoples
+                .ToLookup(x => x.Age);
+
+            foreach (var people in toLookUp)
+            {
+                Console.WriteLine("Age group " + people.Key);
+
+                foreach (People p in people)
+                {
+                    Console.WriteLine("Person name " + p.Name);
+                }
+            }
+        }
+
+        public static void JoinLINQ()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("InnerJoin in LINQ");
+
+            var innerJoin = PeopleList.peoples
+                .Join(GenderList.genderList,
+                humans => humans.GenderId,
+                gender => gender.Id,
+                (humans, gender) => new
+                {
+                    Name = humans.Name,
+                    Sex = gender.Sex
+                });
+
+            foreach (var people in innerJoin)
+            {
+                Console.WriteLine(people.Name + " " + people.Sex);
+            }
+        }
+
+        public static void GroupJoinLINQ()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            var groupJoin = GenderList.genderList
+                .GroupJoin(PeopleList.peoples,
+                p => p.Id,
+                g => g.GenderId,
+                (p, peopleGroup) => new
+                {
+                    Humans = peopleGroup,
+                    GenderFullName = p.Sex
+                });
+
+            foreach (var people in groupJoin)
+            {
+                Console.WriteLine(people.GenderFullName);
+
+                foreach (var name in people.Humans)
+                {
+                    Console.WriteLine(name.Name);
+                }
+            }
+        }
+    }
+}
